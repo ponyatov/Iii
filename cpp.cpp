9dc200b212ce8f58035f8e5975d0b5f065de0fbe
Dmitry Ponyatov <dponyatov@gmail.com>
2017-09-07 01:30:54 +0400

    + simple AST parser w/o eval
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cpp.cpp
@@ -0,0 +1,56 @@
+#include "hpp.hpp"
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main(int argc, char *argv[], char *envp[]) {
+	env = new Env("global");					// create global env
+	Sym* V = new Vector(); env->set("argv",V);	// \ create argv[] vector
+	env->set("argc",new Num(argc));
+	for (int i=0;i<argc;i++)
+		V->push(new Str(argv[i]));				// /
+	env_init();									// continue with env_init
+	return yyparse(); }							// run source parser from stdin
+
+Sym::Sym(string T, string V) { tag = T; val = V; }
+Sym::Sym(string V):Sym("sym",V){}
+Sym::~Sym(){/* does nothing */}
+
+Sym* Sym::push(Sym*o) { nest.push_back(o); return this; }
+Sym* Sym::set(string A,Sym*o) { attr[A]=o; return this; }
+
+string Sym::dump(int depth) {
+	string S = "\n"+pad(depth)+head();									// head
+	for (auto i=attr.begin(),e=attr.end();i!=e;i++)						// attr
+		S += "\n"+pad(depth+1) + i->first + " =" + i->second->dump(depth+2);
+	for (auto j=nest.begin(),e=nest.end();j!=e;j++)						// nest
+		S += (*j)->dump(depth+1);
+	return S; }
+string Sym::head() { ostringstream os;
+	os<<"<"<<tag<<":"<<val<<"> @"<<this; return os.str(); }
+string Sym::pad(int n) { string S;
+	for (int i=0;i<n;i++) S += '\t'; return S; }
+
+Sym* Sym::eval(Sym*E) { return this; }
+
+Str::Str(string V):Sym("str",V){}
+string Str::head() { ostringstream os; os<<"'"<<val<<"' @"<<this; return os.str(); }
+
+Num::Num(string V):Sym("num",V) { val = atof(V.c_str()); }
+Num::Num(int N):Sym("num","") { val = N; }
+string Num::head() { ostringstream os; os<<val<<" @"<<this; return os.str(); }
+
+Op::Op(string V):Sym("op",V){}
+Sym* Op::eval(Sym*E) { return this; }
+
+Vector::Vector():Sym("vector",""){}
+string Vector::head() { ostringstream os; os<<"[] @"<<this; return os.str(); }
+
+Env::Env(string V):Sym("env",V) { next = NULL; }
+Env* env = NULL;
+void env_init() {
+	// meta info
+	env->set("MODULE",new Str(MODULE));
+	env->set("TITLE",new Str(TITLE));
+	env->set("AUTHOR",new Str(AUTHOR));
+	env->set("LICENSE",new Str(LICENSE));
+	env->set("GITHUB",new Str(GITHUB));
+}
---
+++ hpp.hpp
@@ -0,0 +1,59 @@
+#ifndef _H_HPP
+#define _H_HPP
+
+// meta info
+
+#define MODULE	"I"
+#define TITLE	"Interpreter"
+#define AUTHOR	"Dmitry Ponyatov <dponyatov@gmail.com>"
+#define LICENSE	"GNU LesserGPL"
+#define GITHUB	"https://github.com/ponyatov/I"
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+struct Sym {							// === [sym]bol
+	string tag,val;							// tag (class,type) and item value
+	Sym(string T,string V);					// <T:V> constructor
+	Sym(string V);							// <sym:V> constructor
+	virtual ~Sym();							// destructor
+	vector<Sym*> nest; Sym* push(Sym*);		// == nest[]ed elements
+	map<string,Sym*> attr;					// == attr{}ibutes
+	Sym* set(string,Sym*);					// setter
+	virtual string dump(int depth=0);		// == dump object in text form
+	virtual string head();					// <T:V> header in text form
+	string pad(int);						// left pad with tabs
+	virtual Sym* eval(Sym*);				// == evaluate in environment
+	Sym* next=NULL;							// ptr to next env in chain
+
+};
+
+struct Env:Sym { Env(string); };			// environment {table}
+extern Env* env;							// global environment
+extern void env_init();						// init global env
+
+struct Str:Sym { Str(string);				// string
+	string head(); };
+struct Num:Sym { Num(string); float val;	// number
+	Num(int); string head(); };
+
+struct Op:Sym { Op(string); 				// operator
+	Sym* eval(Sym*); };
+
+struct Vector:Sym { Vector();				// [ vec tor ]
+	string head(); };
+
+extern int yylex();							// lexer/parser interface
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }	/* token */
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_HPP
